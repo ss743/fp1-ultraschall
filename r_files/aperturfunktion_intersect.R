0899784d800ac86ab02038427b681e4879f346bd
Saskia <s.bondza@gmx.de>
2016-09-26 09:51:25 +0200

    kk

--- r_files/aperturfunktion_intersect.R
+++ r_files/aperturfunktion_intersect.R
@@ -1,9 +1,9 @@
-g <- 2*pi/0.807
+g <- 2*pi/0.00012926
 apertur <- function(x){sqrt(2.72)+sqrt(0.935)*cos(x*g)+ sqrt(0.6735)*cos(2*x*g)+sqrt(0.4245)*cos(3*x*g)+sqrt(0.1815)*cos(4*x*g)+sqrt(0.04012)*cos(5*x*g)}
-plot (apertur,-0.5,0.5)
+plot (apertur,-0.00005,0.00005)
 max <- sqrt(2.72)+sqrt(0.935)+ sqrt(0.6735)+sqrt(0.4245)+sqrt(0.1815)+sqrt(0.06)
 f1 <- function(x){(4.75938/2)*x^0}
 interf <- function(x){abs(apertur(x)-f1(x))}
-intersect <- optimize(interf, interval=c(-0.06, 0))
-plot(f1, -0.5,0.5, add =  TRUE)
+intersect <- optimize(interf, interval=c(-0.00002, 0))
+plot(f1, -0.00005,0.00005, add =  TRUE)
 #plot(interf, -0.2,0.0)
